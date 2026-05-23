12a26dd31c0d12965bb89f3e68ad3f7f16dd9eee
kariminem <karimwael96@gmail.com>
2026-05-23 17:20:18 +0200

    added functions prototypes, changed linked_list structure

--- push_swap.h
+++ push_swap.h
@@ -16,9 +16,20 @@
 
 typedef struct main_stack
 {
-	t_list	*lst_A;
-	char	*head;
-	char	*tail;
+	t_list	*head;
+	t_list	*tail;
 }main_stack;
 
+void	sa(main_stack *stack);
+void	sb(main_stack *stack);
+void	ss(main_stack *a, main_stack *b);
+void	pb(main_stack *a, main_stack *b);
+void	pa(main_stack *a, main_stack *b);
+void    rra(main_stack   *stack);
+void    rrb(main_stack   *stack);
+void	rrr(main_stack *a, main_stack *b);
+void	ra(main_stack   *stack);
+void	rb(main_stack   *stack);
+void	rr(main_stack *a, main_stack *b);
+void	print_stack(char *stack_name, main_stack *stack);
 #endif
