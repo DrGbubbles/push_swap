7810b0347d1031f5c6fb35532e419c3acbd3e53c
kariminem <karimwael96@gmail.com>
2026-05-23 17:19:42 +0200

    modified the main input parsing logic, handled invalid input

--- push_swap.c
+++ push_swap.c
@@ -1,30 +1,111 @@
 #include "push_swap.h"
 
-int	main(int argc, char **argv)
+static void	ft_new_node(main_stack *list, int *ptr)
 {
-	int	i;
-	int	*ptr;
-	main_stack *stack_A;
 	t_list	*new_node;
 
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	new_node->content = ptr;
+	new_node->next = NULL;
+	if (!list->head)
+		list->head = new_node;
+	else
+		list->tail->next = new_node;
+	list->tail = new_node;
+}
+
+static int	catch_duplicate(main_stack *list, int value)
+{
+	t_list	*current;
+
+	current = list->head;
+	while (current)
+	{
+		if (*(int *)current->content == value)
+			return (1);
+		current = current->next;
+	}
+}
+
+static int	is_valid_int(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	print_stack(char *stack_name, main_stack *stack)
+{
+	t_list	*tmp;
+
+	tmp = stack->head;
+	ft_printf("%s", stack_name);
+	while (tmp)
+	{
+		ft_printf("%d --> ", *(int *)tmp->content);
+		tmp = tmp->next;
+	}
+	ft_printf("NULL\n");
+}
+
+int	main(int argc, char **argv)
+{
+	int			i;
+	int			*ptr;
+	main_stack	*stack_a;
+	main_stack	*stack_b;
+
+	stack_a = malloc(sizeof(main_stack));
+	stack_b = malloc(sizeof(main_stack));
 	i = 1;
+	stack_a->head = NULL;
+	stack_a->tail = NULL;
+	stack_b->head = NULL;
+	stack_b->tail = NULL;
 	if (argc > 1)
 	{
-		
 		while (i < argc)
 		{
-			ft_printf("RECARDO");
+			ptr = malloc(sizeof(int));
 			*ptr = ft_atoi(argv[i]);
-			ft_printf("%d\n", *ptr);
-			new_node = ft_lstnew(ptr);
-			ft_lstadd_back(&stack_A->lst_A, new_node);
-			ft_printf("Input number %i ---- > %d ///// content in node --> %d\n", i, ft_atoi(argv[i]), stack_A->lst_A->content);
+			if (catch_duplicate(stack_a, *ptr) || !is_valid_int(argv[i]))
+			{
+				ft_putstr_fd("INVALID_INPUT\n", 2);
+				ft_lstclear(&(stack_a->head), &free);
+				free(ptr);
+				return (0);
+			}
+			ft_new_node(stack_a, ptr);
 			i++;
 		}
 	}
 	else
-	{
-		ft_printf("No Input!");
-	}
-	ft_lstclear(&(stack_A->lst_A), &free);
+		ft_printf("No Input!\n");
+	
+	// TESTING //
+		print_stack("STACK A : ", stack_a);
+	sa(stack_a);
+	print_stack("STACK A : ", stack_a);
+	pb(stack_a, stack_b);
+	pb(stack_a, stack_b);
+	pb(stack_a, stack_b);
+	print_stack("STACK A : ", stack_a);
+	print_stack("STACK B : ", stack_b);
+	rra(stack_a);
+	print_stack("STACK A : ", stack_a);
+	ra(stack_a);
+	print_stack("STACK A : ", stack_a);
 }
